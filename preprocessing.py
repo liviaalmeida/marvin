0178849b91163a0bfd34e4c7d70dfc78dab3ccd3
liviaalmeida <leave.ah@gmail.com>
2018-08-29 16:37:21 -0300

    Added DATE token and changed punctuation removal

--- preprocessing.py
+++ preprocessing.py
@@ -17,11 +17,13 @@ import string
 # ##### #
 re_brackets = re.compile(r'\{.*\}')
 re_html = re.compile(r'<(\/|\\)?.+?>', re.UNICODE)
+re_dates = re.compile(r'\d{1,2}[/-]\d{1,2}[/-]*[\d{2}\d{4}]*', re.UNICODE)
+
+re_punct = re.compile(r'['+string.punctuation+r']+', re.UNICODE)
+
 re_numbers = re.compile(r'\d', re.UNICODE)
 re_emails = re.compile(r'[^\s]+@[^\s]+', re.UNICODE)
 re_url = re.compile(r'(http|https)://[^\s]+', re.UNICODE)
-
-re_separateDots = re.compile(r'(?<=\.)(?=[^\s])', re.UNICODE)
 
 re_quotes = re.compile(r'(?u)[‘’`′“”"\']', re.UNICODE)
 re_hyphen = re.compile(r'[–-]')
@@ -29,18 +31,17 @@ re_hyphen = re.compile(r'[–-]')
 re_trim = re.compile(r' +', re.UNICODE)
 
 
-def cleanString(text, removePunct):
+def cleanString(text):
     """Apply all regex above to a given string."""
 
     text = text.lower()
 
     text = re_brackets.sub('', text)
     text = re_html.sub(' ', text)
+    text = re_dates.sub('DATE', text)
     text = re_numbers.sub('0', text)
     text = re_emails.sub('EMAIL', text)
     text = re_url.sub('URL', text)
-
-    text = re_separateDots.sub(' ', text)
 
     text = re_quotes.sub('', text)
     text = re_hyphen.sub('', text)
@@ -51,12 +52,12 @@ def cleanString(text, removePunct):
 
 def process(text, removePunct, minWords, removeStop, stopWords):
 
-    text = [cleanString(phrase, removePunct) for phrase in text]
+    text = [cleanString(phrase) for phrase in text]
     tokensList = [nltk.wordpunct_tokenize(phrase) for phrase in text]
     if removePunct:
-        tokensList = [[tk for tk in tokens if tk not in string.punctuation] for tokens in tokensList]
+        tokensList = [[tk for tk in tokens if re_punct.fullmatch(tk) == None] for tokens in tokensList]
     if removeStop:
-        tokensList = [[tk for tk in tokens if tk not in stopWords or tk != '...'] for tokens in tokensList]
+        tokensList = [[tk for tk in tokens if tk not in stopWords] for tokens in tokensList]
     text = [' '.join(tokens) for tokens in tokensList if sum(tk.isalpha() for tk in tokens) >= minWords]
 
     return text
